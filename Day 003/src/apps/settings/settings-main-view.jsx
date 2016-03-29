6c3a76083010cfff29bd3385e29827725925a70b
Tellurium Xia <huairenpp@gmail.com>
2016-03-29 08:08:04 +0800

    Update main view for settings app.

--- Day 003/src/apps/settings/settings-main-view.jsx
+++ Day 003/src/apps/settings/settings-main-view.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import TableView from '../../view/table-view/table-view.jsx';
+import TableViewHeaderFooterView from '../../view/table-view-header-footer-view/table-view-header-footer-view.jsx';
+import TableViewCell from '../../view/table-view-cell/table-view-cell.jsx';
 
 class SettingsMainView extends React.Component {
     constructor(props) {
@@ -9,7 +11,59 @@ class SettingsMainView extends React.Component {
     render() {
         return (
             <TableView>
+                <TableViewHeaderFooterView />
+                <TableViewCell />
+                <TableViewCell />
+                <TableViewCell />
+                <TableViewCell />
+                <TableViewCell />
+                <TableViewCell />
+                <TableViewCell />
 
+                <TableViewHeaderFooterView />
+                <TableViewCell />
+                <TableViewCell />
+                <TableViewCell />
+
+                <TableViewHeaderFooterView />
+                <TableViewCell />
+                <TableViewCell />
+                <TableViewCell />
+                <TableViewCell />
+                <TableViewCell />
+                <TableViewCell />
+                <TableViewCell />
+
+                <TableViewHeaderFooterView />
+                <TableViewCell />
+                <TableViewCell />
+                <TableViewCell />
+
+
+                <TableViewHeaderFooterView />
+                <TableViewCell />
+                <TableViewCell />
+                <TableViewCell />
+                <TableViewCell />
+                <TableViewCell />
+                <TableViewCell />
+                <TableViewCell />
+                <TableViewCell />
+                <TableViewCell />
+
+                <TableViewHeaderFooterView />
+                <TableViewCell />
+                <TableViewCell />
+                <TableViewCell />
+                <TableViewCell />
+                <TableViewCell />
+                <TableViewCell />
+                <TableViewCell />
+
+                <TableViewHeaderFooterView />
+                <TableViewCell />
+
+                <TableViewHeaderFooterView />
             </TableView>
         );
     }
---
+++ Day 003/src/view/table-view-header-footer-view/table-view-header-footer-view.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+require('./table-view-header-footer-view.scss');
+
+class TableViewHeaderFooterView extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return (
+            <div className="table-view-header-footer-view">
+
+            </div>
+        );
+    }
+}
+
+TableViewHeaderFooterView.propTypes = {
+
+};
+
+TableViewHeaderFooterView.defaultProps = {
+
+};
+
+export default TableViewHeaderFooterView;
