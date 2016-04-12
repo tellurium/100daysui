b730d59e3d20c03b9b97102630bb01a8f3774132
Tellurium Xia <huairenpp@gmail.com>
2016-04-12 22:27:10 +0800

    Add lint tools and day 004.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ var path = require('path');
 
 module.exports = {
     entry: {
-        'day003/app': path.resolve(__dirname, 'Day 003/src/index.js')
+        'day003/app': path.resolve(__dirname, 'Day 003/src/index.js'),
+        'day004/app': path.resolve(__dirname, 'Day 004/src/index.js')
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -27,11 +28,20 @@ module.exports = {
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            favicon: false
+        }),
         new HtmlWebpackPlugin({
             favicon: false,
             filename: 'day003/index.html',
-            template: path.resolve(__dirname, 'Day 003/src/index.html')
+            template: path.resolve(__dirname, 'Day 003/src/index.html'),
+            inject: false
+        }),
+        new HtmlWebpackPlugin({
+            favicon: false,
+            filename: 'day004/index.html',
+            template: path.resolve(__dirname, 'Day 004/src/index.html'),
+            inject: false
         }),
         new webpack.DefinePlugin({
             __DEV__: process.env.NODE_DEV === 'true'
